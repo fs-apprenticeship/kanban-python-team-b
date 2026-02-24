90ece1b72e2249c0aec97e070eabdb2bd50992bf
Bryan Grigorie <bryangrigorie@gmail.com>
2026-02-24 13:22:07 -0500

    feat: base navbar

--- board/views.py
+++ board/views.py
@@ -4,10 +4,13 @@ from django.urls import reverse
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        # TODO: replace with board:login once created
+        return redirect(reverse('admin:login'))
     return render(request, "board/index.html", {})
 
 
 def logout(request):
     if request.user.is_authenticated:
         _logout(request)
-    return redirect(reverse('index'))
+    return redirect(reverse('board:index'))
