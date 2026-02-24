24a93fa50274bbaceb6b1d0a5121505d0bb0ee44
Bryan Grigorie <bryangrigorie@gmail.com>
2026-02-24 11:32:18 -0500

    test: add logout test

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import User
+
+
+class LogoutTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.logout_url = reverse('logout')
+        self.email = 'test@flatironschool.com'
+        self.password = 'password'
+        self.user = User.objects.create_user(email=self.email, password=self.password)
+
+    def test_user_logged_out(self):
+        response = self.client.get(self.logout_url, follow=False)
+ 
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
