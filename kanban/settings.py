f89a2afc48ff433e76aeefafd38aa7bf7f2e9c9c
Bryan Grigorie <bryangrigorie@gmail.com>
2026-02-25 14:33:22 -0500

    wip: deploy prep - vercel

--- kanban/settings.py
+++ kanban/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     # Third party
     'django_htmx',
@@ -50,6 +51,7 @@ AUTH_USER_MODEL = "board.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Additional static directories
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
