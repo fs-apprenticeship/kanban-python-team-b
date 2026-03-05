d173c8a9c6c16458e29c28644199f096bea71ffc
ISTAFAMARSHALL <istafa.a.marshall@gmail.com>
2026-03-05 11:21:31 -0500

    Update  urls.py with path to task_modal

--- board/urls.py
+++ board/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("tasks/<uuid:task_id>/modal/", views.task_modal, name="task-modal"),
 ]
