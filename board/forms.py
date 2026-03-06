64a258d1e451215f38a264379a2f79ccf88d6809
ISTAFAMARSHALL <istafa.a.marshall@gmail.com>
2026-03-06 13:14:18 -0500

    Add login required tp protect profile view

--- board/forms.py
+++ board/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth import get_user_model, login_required
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 User = get_user_model()
 
